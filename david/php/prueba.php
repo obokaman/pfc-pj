1c236bda107ffe324e4d94c705b1d3af930bf41d
dvdfib <dvdgarcia.83@gmail.com>
2009-10-07 16:48:27 +0200

    Cambios de lugar: www -> php

---
+++ david/php/prueba.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<?php include("includes.php");?>
+
+<?php
+
+get_users();
+	
+?>
+</body>
+</html>
